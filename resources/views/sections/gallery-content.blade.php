8fc89736d32f500e7b89f92fbd2eb047161911ca
Efren Almanza Lamas <efren.almanza@kingtide.com>
2022-11-19 16:09:16 -0600

    [ADD]ADD services en galeries

--- resources/views/sections/gallery-content.blade.php
+++ resources/views/sections/gallery-content.blade.php
@@ -5,10 +5,15 @@
             <h6 class="section-title bg-white text-center text-primary px-3">Our Projects</h6>
             <h1 class="display-6 mb-4">Learn More About Our Complete Projects</h1>
         </div>
+        <div class="text-center mx-auto mb-5 wow fadeInUp filter-button-group button-group js-radio-button-group" data-wow-delay="0.1s" style="max-width: 600px;">
+        @foreach($services as $service)
+            <button class="section-title bg-white text-center text-primary px-3" data-filter=".{{$service->title }}">{{ $service->title }}</button>
+        @endforeach
+        </div>
         <div class="owl-carousel project-carousel wow fadeInUp" data-wow-delay="0.1s">
             @foreach($galleries as $gallery)
-                <div class="project-item border rounded h-100 p-4" data-dot="{{$gallery->index}}">
-                    <div class="position-relative mb-4">
+                <div class="project-item border rounded h-100 p-4 element-item transition {{$gallery->name}} " data-dot="{{$gallery->index}}"  data-category="post-transition">
+                    <div class="position-relative mb-4" >
                         <img class="img-fluid rounded" src="{{$gallery->image}}" alt="">
                         <a href="{{$gallery->image}}" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
                     </div>
